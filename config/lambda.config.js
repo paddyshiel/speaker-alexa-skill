5f667bed9ed8080ca81c5add2ec35fa192084ec2
Patrick Shiel <pashiel@Patricks-MacBook-Pro-2.local>
2017-02-18 10:22:10 +1100

    PALY3 - Cleanup

--- config/lambda.config.js
+++ config/lambda.config.js
@@ -1,9 +1,6 @@
 const { name, description, version } = require('../package.json');
 
 export default {
-  accessKeyId: 'AKIAJID6GY6R4B3OUFOA',
-  secretAccessKey: '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',
-  role: 'arn:aws:iam::839881462406:role/lambda',
   functionName: name,
   description: `${description} (version ${version})`,
   region: 'us-east-1',
